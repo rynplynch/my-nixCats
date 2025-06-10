2e32715ed245660e35e6a004244256134c804995
Ryan Lynch <rynplynch@gmail.com>
2025-06-10 11:07:58 -0700

    from 'nix flake init -t templates#c-hello'

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "An over-engineered Hello World in C";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixCats.url = "github:BirdeeHub/nixCats-nvim";
+
+  outputs = { self, nixpkgs, nixCats}@inputs:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+      };
+
+      # Provide some binary packages for selected system types.
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (system: let
+        pkgs = import nixpkgs { inherit system; overlays = []; config = {}; };
+
+    in nixCats.utils.mkAllWithDefault (import ./. (inputs // { inherit pkgs; })));
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.default.nixosModule);
+
+      devShells = forAllSystems (system:
+        {
+          default = import ./shell.nix { pkgs = nixpkgsFor.${system}; };
+        });
+
+      # A NixOS module, if applicable (e.g. if the package provides a system service).
+      nixosModules.hello =
+        { pkgs, ... }:
+        {
+          nixpkgs.overlays = [ self.overlay ];
+
+          environment.systemPackages = [ pkgs.hello ];
+
+          #systemd.services = { ... };
+        };
+
+
+    };
+}
