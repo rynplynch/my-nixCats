a4e57a98241a4d5cc72f26002fa8bb80149be2b5
Ryan Lynch <rynplynch@gmail.com>
2025-06-15 12:22:23 -0700

    try to find our init.lua, if found execute it

--- shell.nix
+++ shell.nix
@@ -18,6 +18,12 @@ with pkgs; let
               vim.opt.packpath:prepend(newCfgDir)
               vim.opt.runtimepath:prepend(newCfgDir)
               vim.opt.runtimepath:append(newCfgDir .. "/after")
+              if vim.fn.filereadable(newCfgDir .. "/init.vim") == 1 then
+                vim.cmd.source(newCfgDir .. "/init.vim")
+              end
+              if vim.fn.filereadable(newCfgDir .. "/init.lua") == 1 then
+                dofile(newCfgDir .. "/init.lua")
+              end
             '';
         };
       });
