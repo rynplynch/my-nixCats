ac61681b7d1b05038a4bd13039d7070be0686547
Ryan Lynch <rynplynch@gmail.com>
2025-07-20 22:42:49 -0700

    better bindings for my laptop

--- plugin/debug.lua
+++ plugin/debug.lua
@@ -8,12 +8,12 @@ local dap = require("dap")
 local dapui = require("dapui")
 
 -- Basic debugging keymaps, feel free to change to your liking!
-vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-vim.keymap.set("n", "<leader>B", function()
+vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debug: Start/Continue" })
+vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debug: Step Into" })
+vim.keymap.set("n", "<leader>ds", dap.step_over, { desc = "Debug: Step Over" })
+vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Debug: Step Out" })
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+vim.keymap.set("n", "<leader>dB", function()
    dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 end, { desc = "Debug: Set Breakpoint" })
 
