3b843274b78f3f25ce21693e3811933f517fed36
Ryan Lynch <rynplynch@gmail.com>
2025-07-26 13:16:47 -0700

    use nvim-lspconfig default config for roslyn_ls

--- plugin/csharp.lua
+++ plugin/csharp.lua
@@ -7,3 +7,5 @@ vim.cmd.packadd("vimplugin-treesitter-grammar-c_sharp")
 local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-c_sharp')
 vim.treesitter.language.add('c_sharp', { path = grammar_path .. '/parser/c_sharp.so' })
 vim.treesitter.language.register('c_sharp', { 'cs' })
+
+vim.lsp.enable('roslyn_ls')
