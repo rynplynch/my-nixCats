816b5d80c221d916953867804701c37e32899790
Ryan Lynch <rynplynch@gmail.com>
2025-07-26 13:16:10 -0700

    tell treesitter to use c_sharp grammar for *.cs files

--- plugin/csharp.lua
+++ plugin/csharp.lua
@@ -6,3 +6,4 @@ end
 vim.cmd.packadd("vimplugin-treesitter-grammar-c_sharp")
 local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-c_sharp')
 vim.treesitter.language.add('c_sharp', { path = grammar_path .. '/parser/c_sharp.so' })
+vim.treesitter.language.register('c_sharp', { 'cs' })
