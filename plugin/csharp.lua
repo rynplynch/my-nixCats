8df7ce60053cd04ec01639c2ce930913c7313628
Ryan Lynch <rynplynch@gmail.com>
2025-07-26 13:15:41 -0700

    Add c_sharp grammar to treesitter

--- plugin/csharp.lua
+++ plugin/csharp.lua
@@ -5,3 +5,4 @@ end
 -- configure nix grammar for treesitter
 vim.cmd.packadd("vimplugin-treesitter-grammar-c_sharp")
 local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-c_sharp')
+vim.treesitter.language.add('c_sharp', { path = grammar_path .. '/parser/c_sharp.so' })
