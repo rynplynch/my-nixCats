990e19df72462898bed5773db97df6250cc1c186
Ryan Lynch <rynplynch@gmail.com>
2025-10-20 19:07:25 -0700

    solve for the path of the grammar

--- plugin/org.lua
+++ plugin/org.lua
@@ -5,3 +5,6 @@ end
 -- configure nix grammar for treesitter
 vim.cmd.packadd("vimplugin-treesitter-grammar-org")
 
+-- use nixCats utility function to get grammars install location in the nix store
+local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-org')
+
