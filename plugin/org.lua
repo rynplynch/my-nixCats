85e436515ccd09cb05793092cf245418e622f010
Ryan Lynch <rynplynch@gmail.com>
2025-10-20 19:10:15 -0700

    configure grammar for use

--- plugin/org.lua
+++ plugin/org.lua
@@ -8,3 +8,7 @@ vim.cmd.packadd("vimplugin-treesitter-grammar-org")
 -- use nixCats utility function to get grammars install location in the nix store
 local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-org')
 
+-- configure neovim's native treesitter functionality
+ im.treesitter.language.add('org', { path = grammar_path .. '/parser/org.so' })
+vim.treesitter.language.register('org', { 'org' })
+
