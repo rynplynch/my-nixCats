ea79a254c07f48fda1169437b56f7e27b4300484
Ryan Lynch <rynplynch@gmail.com>
2026-02-25 19:33:23 -0800

    adding rust dependencies
    configure rust language server
    
    activate rust category

---
+++ plugin/rust.lua
@@ -0,0 +1,11 @@
+if not nixCats("rust") or not nixCats("dev") then
+   return
+end
+
+-- configure rust grammar for treesitter
+vim.cmd.packadd("vimplugin-treesitter-grammar-rust")
+local grammar_path = nixCats.pawsible('allPlugins.opt.vimplugin-treesitter-grammar-rust')
+vim.treesitter.language.add('rust', { path = grammar_path .. '/parser/rust.so' })
+vim.treesitter.language.register('rust', { 'rs' })
+
+vim.lsp.enable('rust_analyzer')
