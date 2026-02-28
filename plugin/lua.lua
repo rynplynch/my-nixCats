78bcc0557fa6f2b1ec916c348b7a59da2dccda06
Ryan Lynch <rynplynch@gmail.com>
2026-02-28 12:17:22 -0800

    stop using lazydev; update lua_ls config

--- plugin/lua.lua
+++ plugin/lua.lua
@@ -3,23 +3,52 @@ if not nixCats("lua") or not nixCats("dev") then
    return
 end
 
-vim.cmd.packadd("lazydev.nvim")
-require('lazydev').setup({})
-
 -- this ammends the default nvim-lspconfig configurations
 -- vim.lsp.config will search the /lsp directory, finding the file name 'lua_ls'
-vim.lsp.config['lua_ls'] = {
+vim.lsp.config('lua_ls', {
+   on_init = function(client)
+      if client.workspace_folders then
+         local path = client.workspace_folders[1].name
+         if
+             path ~= vim.fn.stdpath('config')
+             and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+         then
+            return
+         end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+         runtime = {
+            -- Tell the language server which version of Lua you're using (most
+            -- likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Tell the language server how to find Lua modules same way as Neovim
+            -- (see `:h lua-module-load`)
+            path = {
+               'lua/?.lua',
+               'lua/?/init.lua',
+            },
+         },
+         -- Make the server aware of Neovim runtime files
+         workspace = {
+            checkThirdParty = true,
+            library = {
+               vim.env.VIMRUNTIME,
+            },
+         },
+      })
+   end,
    settings = {
       Lua = {
          runtime = {
             version = 'LuaJIT',
          },
          diagnostics = {
-            globals = { "nixCats", "vim" }
+            globals = { "nixCats" }
          },
-         capabilities = require('blink-cmp').get_lsp_capabilities(),
       }
    }
-}
+})
+
 
 vim.lsp.enable('lua_ls')
