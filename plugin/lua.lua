0112088b62ce472a76da58b1a01aa4a2e246e8e7
Ryan Lynch <rynplynch@gmail.com>
2026-03-08 21:05:31 -0700

    move logic for Lua debuging to lua.nix

--- plugin/lua.lua
+++ plugin/lua.lua
@@ -52,3 +52,33 @@ vim.lsp.config('lua_ls', {
 
 
 vim.lsp.enable('lua_ls')
+
+-- if lua is enable, configure debugger
+if nixCats("debug") then
+   local dap = require("dap")
+   dap.configurations.lua = {
+      {
+         type = 'nlua',
+         request = 'attach',
+         name = "Attach to running Neovim instance",
+      }
+   }
+
+   dap.adapters.nlua = function(callback, config)
+      callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
+   end
+
+   vim.keymap.set('n', '<leader>dl', function()
+      require "osv".launch({ port = 8086 })
+   end, { noremap = true })
+
+   vim.keymap.set('n', '<leader>dw', function()
+      local widgets = require "dap.ui.widgets"
+      widgets.hover()
+   end)
+
+   vim.keymap.set('n', '<leader>df', function()
+      local widgets = require "dap.ui.widgets"
+      widgets.centered_float(widgets.frames)
+   end)
+end
