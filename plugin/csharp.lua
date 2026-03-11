5145231b61bcdd384c31c8ffafa0931324e88ef1
Ryan Lynch <rynplynch@gmail.com>
2026-03-10 23:28:46 -0700

    correction?

--- plugin/csharp.lua
+++ plugin/csharp.lua
@@ -3,7 +3,7 @@ if not nixCats("csharp") or not nixCats("dev") then
 end
 
 vim.lsp.config('roslyn_ls', {
-   buf_attach_client = function(bufnr, client_id)
+   on_attach = function(bufnr, client_id)
       vim.keymap.set("n", "<C-e>c", function()
          local user_input = vim.fn.input({ prompt = "Enter input: ", completion = "dir_in_path" })
          vim.api.nvim_cmd({ cmd = "te", args = { "dotnet run --project " .. user_input } }, {})
