2c36f0a845e631ba42de6963090488fbbe5d7593
Ryan Lynch <rynplynch@gmail.com>
2026-03-10 22:21:02 -0700

    key mapping for running dotnet project in given dir

--- plugin/csharp.lua
+++ plugin/csharp.lua
@@ -2,6 +2,15 @@ if not nixCats("csharp") or not nixCats("dev") then
    return
 end
 
+vim.lsp.config('roslyn_ls', {
+   buf_attach_client = function(bufnr, client_id)
+      vim.keymap.set("n", "<C-e>c", function()
+         local user_input = vim.fn.input({ prompt = "Enter input: ", completion = "dir_in_path" })
+         vim.api.nvim_cmd({ cmd = "te", args = { "dotnet run --project " .. user_input } }, {})
+      end, bufnr)
+   end
+})
+
 --- configure nix grammar for treesitter
 vim.cmd.packadd("nvim-treesitter-grammar-c_sharp")
 local grammar_path = nixCats.pawsible('allPlugins.opt.nvim-treesitter-grammar-c_sharp')
