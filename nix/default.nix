0832e9500769a1283ddb19f9db9157eb4628d345
Ryan Lynch <rynplynch@gmail.com>
2026-06-30 14:20:03 -0700

    devShell with nvim config dir set to current working dir

--- nix/default.nix
+++ nix/default.nix
@@ -18,25 +18,6 @@ inputs:
     default = config.nvim-lib.pluginsFromPrefix "plugins-" inputs;
   };
 
-  # choose a directory for your config.
-  config.settings.config_directory = ./.;
-  # you can also use an impure path!
-  # config.settings.config_directory = lib.generators.mkLuaInline "vim.fn.stdpath('config')";
-  # config.settings.config_directory = "/home/<USER>/.config/nvim";
-  # If you do that, it will not be provisioned by nix, but it will have normal reload for quick edits!
-
-  # If you want to install multiple neovim derivations via home.packages or environment.systemPackages
-  # in order to prevent path collisions:
-
-  # set this to true:
-  # config.settings.dont_link = true;
-
-  # and make sure these dont share values:
-  # config.binName = "nvim";
-  # config.settings.aliases = [ ];
-
-  # To add a wrapped $out/bin/${config.binName}-neovide to the resulting neovim derivation
-  # config.hosts.neovide.nvim-host.enable = true;
 
   # You can declare your own options!
   options.settings.colorscheme = lib.mkOption {
