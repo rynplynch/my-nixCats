16779aaefb9c6722492184f4eaa3cc3f2979afb8
Ryan Lynch <rynplynch@gmail.com>
2026-08-23 08:26:47 -0700

    old files that no longer get called

--- plugin.old/debug.lua
+++
@@ -1,8 +0,0 @@
-if not nixCats("debug") then
-   return
-end
--- NOTE: Base debugging setup
--- TODO: To run a debugger, set up or install a language specific config
-
-require("dapui").setup()
-require("nvim-dap-virtual-text").setup()
